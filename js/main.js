0369b89c66448e9ab687c7798d21ea1db50d9bc6
Gusales <dev.gussales@gmail.com>
2025-07-06 22:15:42 -0300

    Trabalho parcialmente concluido, falta finalizar o formulário de contato

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ function toogleTheme() {
   const body = document.body;
   body.classList.toggle('light');
 }
+
+function showAccordion(accordionId) {
+  const accordionDivs = Array.from(document.querySelectorAll(".accordion-pannel"))
+
+  if (accordionDivs.length > 0) {
+    const accordionDiv = accordionDivs[accordionId - 1]
+    console.log(accordionDiv)
+
+    if (accordionDiv.style.maxHeight) {
+      accordionDiv.style.maxHeight = null;
+    } else {
+      accordionDiv.style.maxHeight = (accordionDiv.scrollHeight + 32) + "px";
+    }
+
+    accordionDiv.classList.toggle("active")
+  }
+}
